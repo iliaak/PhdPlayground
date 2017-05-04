d1ccf8b55e336e3b4d4125b0e1950ea0964d2537
Peter Bourgonje <peter@apollo.ling.uni-potsdam.de>
2017-05-04 16:02:13 +0200

    adding PCC parser for backup purposes

---
+++ PCCParser.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import string
+from collections import defaultdict
+from optparse import OptionParser
+import os
+import codecs
+import lxml.etree
+from itertools import chain
+
+
+class EDU:
+    def __init__(self, text, isConnective):
+        self.text = text
+        self.isConnective = isConnective
+
+
+
+def parseFile(pccxml, file2edulist):
+
+    xmlParser = lxml.etree.XMLParser(strip_cdata = False, resolve_entities = False, encoding = 'utf-8')
+    tree = lxml.etree.parse(pccxml, parser = xmlParser)
+    l = extractEDUs(tree.getroot(), [])
+    file2edulist[pccxml] = l
+    return file2edulist
+    
+
+def extractEDUs(node, l):
+
+    if node.text:
+        conn = False
+        if node.tag == 'connective':
+            conn = True
+        textContent = node.text.strip()
+        if textContent: # filtering out (empty) newlines
+            edu = EDU(textContent, conn)
+            l.append(edu)
+            #conn = False
+        conn = False
+        if len(node):
+            for subnode in node.getchildren():
+                extractEDUs(subnode, l)
+        
+        if node.tail: # filtering out (empty) newlines
+            if node.tail.strip():
+                l.append(EDU(node.tail.strip(), conn))
+
+    return l
+    
+
+def getInputfiles(infolder):
+
+    filelist = []
+    for f in os.listdir(infolder):
+        abspathFile = os.path.abspath(os.path.join(infolder, f))
+        filelist.append(abspathFile)
+    return filelist
+
+
+def printPlaintext(file2edulist):
+
+    for f in file2edulist:
+        print('++++++++++ %s ++++++++++' % f)
+        print(re.sub(r'\s+', ' ', ' '.join([x.text for x in file2edulist[f]])))
+        print('\n')
+
+def printConllGold(file2edulist):
+
+    for f in file2edulist:
+        print('++++++++++ %s ++++++++++' % f)
+        # assuming that everything is (still) whitespace-tokenized
+        for edu in file2edulist[f]:
+            tokens = edu.text.split(' ')
+            cb = edu.isConnective
+            for i, t in enumerate(tokens):
+                word, val = t, 'O'
+                if cb:
+                    if i == 0:
+                        val = 'B-connective'
+                    else:
+                        val = 'I-connective'
+                print('%s\t%s' % (word, val))
+        print('\n')
+            
+if __name__ == '__main__':
+
+    parser = OptionParser('Usage: %prog -options')
+    parser.add_option('-f', '--xmlFolder', dest='xmlFolder', help='specify PCC xml folder')
+
+    options, args = parser.parse_args()
+
+    if not options.xmlFolder:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    inputfiles = getInputfiles(options.xmlFolder)
+        
+    file2edulist = defaultdict(list)
+    for f in inputfiles:
+        file2edulist = parseFile(f, file2edulist)
+
+    
+    #printPlaintext(file2edulist)
+
+    printConllGold(file2edulist) #TODO: debug this (to see if everything is correct)
+    
+
