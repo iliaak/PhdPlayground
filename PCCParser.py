5e8f41cb6883b37e5754c971b7558ad054493140
Peter Bourgonje <peter@apollo.ling.uni-potsdam.de>
2017-05-18 13:55:36 +0200

    successfully combined connectors, syntax and rst annotation layers of PCC

--- PCCParser.py
+++ PCCParser.py
@@ -15,13 +15,9 @@ Potsdam Commentary Corpus Parser for some initial playing around with the format
 """
 connectorTokenId = 1
 syntaxTokenId = 0 # bit ugly, but because of zero-index list...
+rstTokenId = 0 # idem
 
-"""
-class EDU:
-    def __init__(self, text, isConnective):
-        self.text = text
-        self.isConnective = isConnective
-"""
+
 
 class DiscourseToken:
     def __init__(self, tokenId, token):
@@ -30,6 +26,10 @@ class DiscourseToken:
     # TODO: read up on proper way of doing this, think I'm mixing java and python too much
     def setIntOrExt(self, val):
         self.intOrExt = val
+    def setMultiWordBoolean(self, val):
+        self.isMultiWord = val
+    def setConnectiveBoolean(self, val):
+        self.isConnective = val
     def setUnitId(self, val):
         self.unitId = val
     def setRelation(self, val):
@@ -44,7 +44,14 @@ class DiscourseToken:
         self.terminalsId = val
     def setMorph(self, val):
         self.morph = val
+    def setRstSegmentId(self, val):
+        self.rstSegmentId = val
+    def setRstParent(self, val):
+        self.rstParent = val
+    def setRelname(self, val):
+        self.relname = val
 
+        
         
 def parseConnectorFile(connectorxml):
 
@@ -69,12 +76,11 @@ def parseSyntaxFile(syntaxxml, tokenlist):
             for t in terminalsNode:
                 sToken = t.get('word')
                 dt = tokenlist[syntaxTokenId]
-                print('dt token:', dt.token, '\t', 'sToken:', sToken)
+                #print('dt token:', dt.token, '\t', 'sToken:', sToken)
                 if not sToken == dt.token:
-                    sys.stderr.write('FATAL ERROR: Tokens do not match in %s: %s(%s) vs. %s(%s).\n' % (syntaxxml, sToken, str(syntaxTokenId), tokenlist[syntaxTokenId].token, str(tokenlist[syntaxTokenId].tokenId))) # TODO debug this for more files
+                    sys.stderr.write('FATAL ERROR: Tokens do not match in %s: %s(%s) vs. %s(%s).\n' % (syntaxxml, sToken, str(syntaxTokenId), tokenlist[syntaxTokenId].token, str(tokenlist[syntaxTokenId].tokenId)))
                     sys.exit(1)
 
-                # TODO: continue here. This crashes on maz-10175 because of a nested structure, fix connector text extraction
                 dt.setLemma(t.get('lemma'))
                 dt.setPOS(t.get('pos'))
                 dt.setTerminalsId(t.get('id'))
@@ -84,17 +90,36 @@ def parseSyntaxFile(syntaxxml, tokenlist):
                 syntaxTokenId += 1
     syntaxTokenId = 0 # reset at end of file          
     return tokenlist
-            
-"""
-def parseConnectorFile(pccxml, file2edulist):
 
-    xmlParser = lxml.etree.XMLParser(strip_cdata = False, resolve_entities = False, encoding = 'utf-8')
-    tree = lxml.etree.parse(pccxml, parser = xmlParser)
-    l = extractEDUs(tree.getroot(), [])
-    file2edulist[pccxml] = l
-    return file2edulist
-"""
-def createDiscourseToken(token, node, conn):
+def parseRSTFile(rstxml, tokenlist):
+
+    global rstTokenId # not sure why I made this global anymore, think it's not needed in this case
+    xmlParser = lxml.etree.XMLParser(strip_cdata = False, resolve_entities = False, encoding = 'utf-8', remove_comments = True)
+    tree = lxml.etree.parse(rstxml, parser = xmlParser)
+    body = tree.getroot().find('.//body')
+    for node in body:
+        if node.tag == 'segment':
+            sId = node.get('id')
+            sParent = node.get('parent')
+            sRelname = node.get('relname')
+            tokens = node.text.split()
+            for token in tokens:
+                dt = tokenlist[rstTokenId]
+                if not token == dt.token:
+                    sys.stderr.write('FATAL ERROR: Tokens do not match in %s: %s(%s) vs. %s(%s).\n' % (rstxml, sToken, str(rstTokenId), tokenlist[rstTokenId].token, str(tokenlist[rstTokenId].tokenId)))
+                    sys.exit(1)
+                dt.setRstSegmentId(sId)
+                dt.setRstParent(sParent)
+                dt.setRelname(sRelname)
+                rstTokenId += 1
+            # may want to add non-terminal rst tree info here
+            
+    rstTokenId = 0
+    return tokenlist
+
+
+  
+def createDiscourseToken(token, node, conn, multiWordBool):
 
     global connectorTokenId
     dt = DiscourseToken(connectorTokenId, token)
@@ -115,60 +140,47 @@ def createDiscourseToken(token, node, conn):
                 segmentType = node.getparent().get('type')
         dt.setIntOrExt(segmentType)
     elif segmentType == 'connective':
+        
         dt.setRelation(node.get('relation'))
+    dt.setMultiWordBoolean(multiWordBool)
+    dt.setConnectiveBoolean(conn) # a bit redundant, because the type also contains this info, but this is easier for if checks...
     connectorTokenId += 1
     return dt
     
 
 def extractTokens(node, l):
 
+    conn = False
     if node.text:
-        conn = False
         if node.tag == 'connective':
             conn = True
         textContent = node.text.strip()
         if textContent:
             tokens = textContent.split()
+            multiWordBool = False
+            if len(tokens) > 1:
+                multiWordBool = True
             for token in tokens:
-                dt = createDiscourseToken(token, node, conn)
+                dt = createDiscourseToken(token, node, conn, multiWordBool)
                 l.append(dt)
         conn = False
-        if len(node):
-            for subnode in node.getchildren():
-                extractTokens(subnode, l)
+    if len(node):
+        for subnode in node.getchildren():
+            extractTokens(subnode, l)
 
-        if node.tail:
-            if node.tail.strip():
-                tokens = node.tail.strip().split()
-                for token in tokens:
-                    dt = createDiscourseToken(token, node, conn)
-                    l.append(dt)
-
-    return l
-
-"""                    
-def extractEDUs(node, l):
-
-    if node.text:
-        conn = False
-        if node.tag == 'connective':
-            conn = True
-        textContent = node.text.strip()
-        if textContent: # filtering out (empty) newlines
-            edu = EDU(textContent, conn)
-            l.append(edu)
-            #conn = False
-        conn = False
-        if len(node):
-            for subnode in node.getchildren():
-                extractEDUs(subnode, l)
+    if node.tail:
+        if node.tail.strip():
+            tokens = node.tail.strip().split()
+            multiWordBool = False
+            if len(tokens) > 1:
+                multiWordBool = True
+            for token in tokens:
+                dt = createDiscourseToken(token, node, conn, multiWordBool)
+                l.append(dt)
         
-        if node.tail: # filtering out (empty) newlines
-            if node.tail.strip():
-                l.append(EDU(node.tail.strip(), conn))
-
     return l
-""" 
+
+
 
 def getInputfiles(infolder):
 
@@ -226,7 +238,7 @@ def printStats(file2edulist):
     print('INFO: %i connector tokens.' % ct)
     
 
-def getFileVersionsDict(connectorFiles, syntaxFiles):
+def getFileVersionsDict(connectorFiles, syntaxFiles, rstFiles):
 
     d = defaultdict(lambda : defaultdict(str))
     for f in connectorFiles:
@@ -235,6 +247,9 @@ def getFileVersionsDict(connectorFiles, syntaxFiles):
     for f in syntaxFiles:
         name = os.path.splitext(os.path.basename(f))[0]
         d[name]['syntax'] = f
+    for f in rstFiles:
+        name = os.path.splitext(os.path.basename(f))[0]
+        d[name]['rst'] = f
 
     # sanity check:
     die = False
@@ -245,56 +260,106 @@ def getFileVersionsDict(connectorFiles, syntaxFiles):
         if not len(d[name]['syntax']):
             sys.stderr.write('ERROR: Could not find annotation layer <syntax> for %s.\n' % name)
             die = True
+        if not len(d[name]['rst']):
+            sys.stderr.write('ERROR: Could not find annotation layer <rst> for %s.\n' % name)
+            die = True
     if die:
         sys.stderr.write('FATAL ERROR: Please investigate annotation layers.\n')
         sys.exit(1)
 
     return d
+
+def debugPrint(file2tokens):
+
+    for f in file2tokens:
+        print(f)
+        for dt in file2tokens[f]:
+            tokenId = dt.tokenId
+            token = dt.token
+            intOrExt = '___'
+            if hasattr(dt, 'intOrExt'): # intOrExt is only specified for EDUs (not for connectives)
+                intOrExt = dt.intOrExt
+            relation = '___'    
+            if (hasattr(dt, 'relation')): # relation is only specified for connectives
+                relation = dt.relation
+            sType = dt.segmentType
+            lemma = dt.lemma
+            pos = dt.pos
+            rstParent = dt.rstParent
+            rstRelation = dt.relname
+            
+            print('\t%s %s %s %s %s %s %s %s' % (str(tokenId), token, sType, pos, intOrExt, relation, lemma, rstRelation))
+            
+
+def printConnectiveStats(file2tokens):
+
+    singleWordConnectives = defaultdict(int)
+    multiWordConnectives = defaultdict(int)
+
+    for f in file2tokens:
+        for dt in file2tokens[f]:
+            if dt.isConnective:
+                if not dt.isMultiWord:
+                    singleWordConnectives[dt.token] += 1
+                else:
+                    pass # do multiword stuff here (getting i+1 etc.)
+
+
+    connpostags = defaultdict(lambda : defaultdict(float))
+    nonconnpostags = defaultdict(lambda : defaultdict(float))
+    for swc in singleWordConnectives:
+        nonconnreading = 0
+        connreading = 0
+    
+        for f in file2tokens:
+            for dt in file2tokens[f]:
+                if dt.token == swc:
+                   if dt.isConnective:
+                       connreading += 1
+                   else:
+                       nonconnreading += 1
+                # TODO: include pos tag, lemma, rst-relation etc here
+                
+        print('Single Word Connective:', swc)
+        print('\tconnective ratio: %i of %i' % (connreading, connreading + nonconnreading))  
+        print('\n')
         
+        
+            
 if __name__ == '__main__':
 
     parser = OptionParser('Usage: %prog -options')
     parser.add_option('-c', '--connectorsFolder', dest='connectorsFolder', help='specify PCC connectors folder')
     parser.add_option('-s', '--syntaxFolder', dest='syntaxFolder', help='specify PCC syntax folder')
+    parser.add_option('-r', '--rstFolder', dest='rstFolder', help='specify PCC RST folder')
 
     options, args = parser.parse_args()
 
-    if not options.connectorsFolder or not options.syntaxFolder:
+    if not options.connectorsFolder or not options.syntaxFolder or not options.rstFolder:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
     connectorfiles = getInputfiles(options.connectorsFolder)
     syntaxfiles = getInputfiles(options.syntaxFolder)
-
-    fileversions = getFileVersionsDict(connectorfiles, syntaxfiles) # makes a dict with filename 2 connectors, syntax, etc.
+    rstfiles = getInputfiles(options.rstFolder)
     
+    fileversions = getFileVersionsDict(connectorfiles, syntaxfiles, rstfiles) # makes a dict with filename 2 connectors, syntax, etc.
     
     file2tokens = defaultdict(list)
     for name in fileversions:
         tokenlist = parseConnectorFile(fileversions[name]['connectors'])
         tokenlist = parseSyntaxFile(fileversions[name]['syntax'], tokenlist)
-        
+        tokenlist = parseRSTFile(fileversions[name]['rst'], tokenlist)
         file2tokens[name] = tokenlist
-        print('DEBUG: parsed %s' % name)
-        """
-        for dt in tokenlist:
-            print('token id:', dt.tokenId)
-            print('token:', dt.token)
-            print('type:', dt.segmentType)
-            print('unitId:', dt.unitId)
-            if dt.segmentType == 'unit':
-                print('intOrExt:', dt.intOrExt)
-            elif dt.segmentType == 'connective':
-                print('relation:', dt.relation)
-            print('\n')
-        """
-        #print(' '.join([t.token for t in tokenlist]))
-        
-    #for fname in file2tokens:
-        #print(fname)
-        #for dt in file2tokens[fname]:
-            #print('\t%s %s %s %s' % (dt.token, dt.segmentType, dt.pos, dt.lemma))
+
+
+    debugPrint(file2tokens)
     
+    #printConnectiveStats(file2tokens)
+
+    
+            
+            
     # not sure if these printing functions still work after recent changes              
     #printPlaintext(file2edulist)
 
@@ -302,10 +367,5 @@ if __name__ == '__main__':
     
     ##printStats(file2edulist)
 
-    #TODO: combine this with other annotation layers, to print (for learning/inspiration purposes)
-    # conll format output with features (i.e. pos-tag of connective, dep parse info, etc.)
-    # TODO: fix structure based on EDU ID, isInternal, isExternal, etc...
-    # battle plan: assuming that everything is tokenised (such that whitespace splitting results in proper tokens)
-    # go through the connector file and the syntax file (rst file is the one we want to construct eventually, so maybe look into that at
-    # a later point) , and map every token to either its unit id (connector file) or graph and t(erminal) id (syntax file)
-    # the, going through the list of tokens top to bottom, I can link units (connectors) and postags, lemmas, etc (syntax)
+    
+    
