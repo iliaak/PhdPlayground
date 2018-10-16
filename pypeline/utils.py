6d7887ee3350168610c7926a9ba230932fcbb314
PeterBourgonje <bourgonje@gmail.com>
2018-10-16 16:51:15 +0200

    adding utils backup

--- pypeline/utils.py
+++ pypeline/utils.py
@@ -72,3 +72,22 @@ def mergePhrasalConnectives(l):
             l2.append(l[i])
     l = l2
     return l
+
+def getPostagFromTree(ptree, tokenIndex):
+    # had I known it was this simple, wouldn't have to dedicate a function to it...
+    return ptree.pos()[tokenIndex][1]
+
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
