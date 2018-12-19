a8554e0681fbba0cde11b51eab27a3ec1632d479
Peter Bourgonje <bourgonje@uni-potsdam.de>
2018-12-19 16:20:33 +0100

    moving getInputfiles, loadEmbeddings and others to utils file because it was all over the place, and adding script to filter external embeddings for data such that loading into memory goes much faster (provided that the entire data set is known)

---
+++ filterEmbeddingsForData.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+
+import sys
+import re
+import string
+from collections import defaultdict
+from optparse import OptionParser
+import os
+import codecs
+import utils
+
+
+
+def data2tokens(data):
+
+    tokens = []
+    for f in utils.getInputfiles(data):
+        for line in codecs.open(f).readlines():
+            for token in line.split():
+                tokens.append(token)
+    return set(tokens)
+
+
+def filterEmbd(embd, tokens):
+
+    d = {}
+    for e in embd:
+        if e in tokens:
+            d[e] = embd[e]
+    return d
+
+
+if __name__ == '__main__':
+   
+    parser = OptionParser("usage: %prog corpus")
+    parser.add_option("-v", "--embeddings", dest="embeddings", help="specify embeddings file (ending in .vec, or at least white-space separated, where first column is the wor)...")
+    parser.add_option("-d", "--data", dest="data", help="specify data folder, with pre-tokenized input (white space splitting returns tokens)...")
+
+    options, args = parser.parse_args()
+    
+    if not options.embeddings or not options.data:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    embd = utils.loadExternalEmbeddings(options.embeddings)
+    tokens = data2tokens(options.data)
+        
+    filtered = filterEmbd(embd, tokens)
+    
+    outf = codecs.open(os.path.splitext(options.embeddings)[0] + '.filtered.vec', 'w')
+
+    dimensions = codecs.open(options.embeddings, 'r').readline()
+    outf.write(dimensions)
+    for e in filtered:
+        outf.write('%s %s\n' % (e, ' '.join([str(x) for x in filtered[e]])))
+        
+    outf.close()
---
+++ utils.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from collections import defaultdict
+import re
+import numpy
+import codecs
+import time
+
+
+def loadExternalEmbeddings(embfile):
+
+    starttime = time.time()
+    sys.stderr.write('INFO: Loading external embeddings from %s.\n' % embfile)
+    ed = defaultdict()
+    #"""
+    with codecs.open(embfile, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            values = line.split()
+            ed[values[0]] = numpy.array([float(x) for x in values[1:]])
+    #"""
+    endtime = time.time()
+    sys.stderr.write('INFO: Done loading embeddings. Took %s seconds.\n' % (str(endtime - starttime)))
+    return ed
+
+
+def getInputfiles(infolder):
+
+    filelist = []
+    for f in os.listdir(infolder):
+        abspathFile = os.path.abspath(os.path.join(infolder, f))
+        filelist.append(abspathFile)
+    return filelist
